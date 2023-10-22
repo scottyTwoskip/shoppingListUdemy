007ff1f1184a2e073f9196c8436b4ba98a05b022
scottyTwoskip <scottyeansor@gmail.com>
2023-10-22 08:17:48 -0700

    added remove function to cleare buttons and red x icon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearButton = document.getElementById('clear')
 
 function addItem(e) {
     e.preventDefault()
@@ -35,7 +36,19 @@ function createIcon(classes) {
     icon.className = classes
     return icon
 }
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove()
+    }
+}
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+}
 //event listners
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearButton.addEventListener('click', clearItems)
 
 //added comment for test
